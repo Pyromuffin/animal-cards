8e22e9c0528cbd631058e4d6d3e5488b93a6eb46
Loveschach <codymalach@hotmail.com>
2024-01-27 22:03:54 -0800

    Proper card association

--- Deck.cs
+++ Deck.cs
@@ -29,7 +29,7 @@ public partial class Deck : Sprite2D
 		drawPile.Clear();
 		discardPile.Clear();
 
-		for(int i = 0; i < initialDeckSize; i ++){
+		for(int i = 0; i < cardData.Length; i ++){
 			drawPile.Enqueue(Random.Shared.Next() % cardData.Length);
 		}
 	}
@@ -70,17 +70,16 @@ public partial class Deck : Sprite2D
 		cardData = new CardData[lines.Length];
 
 		int index = 0;
-		foreach(var line in lines){
+		foreach(var card in AllCards){
+			var line = lines[card.tableIndex - 1];
 			var fields = line.Split(",");
-			var uid = fields[0].ToInt();
 			var name = fields[1];
 			var desc = fields[2];
-			var effect = AllCards[uid -1];
 
 			var data = new CardData();
 			data.name = name;
 			data.description = desc;
-			data.effect = effect;
+			data.effect = card;
 			cardData[index] = data;
 			index++;
 		}
@@ -92,42 +91,54 @@ public partial class Deck : Sprite2D
 	{
 		AllCards = new List<Playable>();
 		Playable card = new MultiplierDamage( new DamageData[] { new DamageData( PatronTag.Water ), new DamageData( PatronTag.Bird ) } );
+		card.tableIndex = 3;
 		AllCards.Add( card );
 
 		Playable card2 = new MultiplierDamage( new DamageData[] { new DamageData( PatronTag.Water ), new DamageData( PatronTag.Mammal ) } );
+		card2.tableIndex = 4;
 		AllCards.Add( card2 );
 
 		Playable card3 = new MultiplierDamage( new DamageData[] { new DamageData( PatronTag.Flying ), new DamageData( PatronTag.Carnivore ) } );
+		card3.tableIndex = 5;
 		AllCards.Add( card3 );
 
 		Playable card4 = new MultiplierDamage( new DamageData[] { new DamageData( PatronTag.Flying ), new DamageData( PatronTag.Bird ) } );
+		card4.tableIndex = 6;
 		AllCards.Add( card4 );
 
 		Playable card5 =  new MultiplierDamage( new DamageData[] { new DamageData( PatronTag.Herbivore, 2 ) } );
+		card5.tableIndex = 7;
 		AllCards.Add( card5 );
 
 		Playable card6 = new MultiplierDamage( new DamageData[] { new DamageData( PatronTag.ColdBlooded, 2 ) } );
+		card6.tableIndex = 8;
 		AllCards.Add( card6 );
 
 		Playable card7 = new MultiplierDamage( new DamageData[] { new DamageData( PatronTag.Ground ), new DamageData( PatronTag.Carnivore ) } );
+		card7.tableIndex = 9;
 		AllCards.Add( card7 );
 
 		Playable card8 =  new MultiplierDamage( new DamageData[] { new DamageData( PatronTag.Ground, 3 ), new DamageData( PatronTag.Bird, -1 ), new DamageData( PatronTag.ColdBlooded, -1 ) } );
+		card8.tableIndex = 10;
 		AllCards.Add( card8 );
 
 		Playable card9 = new MultiplierDamage( new DamageData[] { new DamageData( PatronTag.Herbivore ), new DamageData( PatronTag.Mammal ) } );
+		card9.tableIndex = 11;
 		AllCards.Add( card9 );
 
 		Playable card10 = new SetupMultiplierDamage( new DamageData[0], 
 			new DamageData[] { new DamageData( PatronTag.Herbivore, 3 ), new DamageData( PatronTag.Carnivore, -3 ) } );
+		card10.tableIndex = 13;
 		AllCards.Add( card10 );
 
 		Playable card11 = new SetupMultiplierDamage( new DamageData[] { new DamageData( PatronTag.Water, -3 ) }, 
 			new DamageData[] { new DamageData( PatronTag.Mammal, 2 ), new DamageData( PatronTag.ColdBlooded, 2 ) } );
+		card11.tableIndex = 15;
 		AllCards.Add( card11 );
 
 		Playable card12 = new SetupMultiplierDamage( new DamageData[] { new DamageData( PatronTag.Flying, -2 ) }, 
 			new DamageData[] { new DamageData( PatronTag.Ground, 2 ), } );
+		card12.tableIndex = 16;
 		AllCards.Add( card12 );
 	}
 }
