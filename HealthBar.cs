398ef3088f55a8d9761e34eedd54fbeb0bb80f18
Loveschach <codymalach@hotmail.com>
2024-01-28 01:01:37 -0800

    Health numbo

--- HealthBar.cs
+++ HealthBar.cs
@@ -4,11 +4,13 @@ using System;
 public partial class HealthBar : Node2D
 {
 	[Export] public Node2D fill;
+	[Export] public Label text;
 	[Export] public int fullPosition, emptyPosition;
 
 	public void SetHealth(int current, int max) {
 		var percent = (float)current / max;
 		var pos = Mathf.Lerp(emptyPosition, fullPosition, percent);
 		fill.Position = new Vector2(pos, 0);
+		text.Text = current + "/" + max;
 	}
 }
